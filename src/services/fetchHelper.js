eec16c01c5e1ebb115171d69d93c47b77888c7e4
julio-silveira <julioqueiroz15@gmail.com>
2022-10-24 17:38:41 -0300

    Ajustes nos nomes dos componentes e criado o componente de Loading

--- src/services/fetchHelper.js
+++ src/services/fetchHelper.js
@@ -1,5 +1,6 @@
 // Meals api https://www.themealdb.com/api.php
 // Para categorias type = c, para nacionalidade type = a, para ingredientes type = i
+
 export const fetchMeal = async (type) => {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?${type}=list`);
   const data = await response.json();
@@ -12,4 +13,18 @@ export const fetchIngredientImage = async (ingredientName) => {
   const data = await response.json();
   return data;
 };
+export const fetchMealById = async (mealId) => {
+  const response = await fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+  const data = await response.json();
+  return data;
+};
+
 // drinks API https://www.thecocktaildb.com/api.php
+
+export const fetchDrinkById = async (drinkId) => {
+  const response = await fetch(
+    `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`,
+  );
+  const data = await response.json();
+  return data;
+};
