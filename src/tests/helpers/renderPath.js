41973a48408708b1bf0c353a8f3edd7a0f8e1e74
Cristiane Kizelevicius <cristiane@samaan.com.br>
2022-10-25 20:09:33 -0300

    finalizando requesitos  de 2 a 6

--- src/tests/helpers/renderPath.js
+++ src/tests/helpers/renderPath.js
@@ -3,13 +3,16 @@ import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { render } from '@testing-library/react';
 import App from '../../App';
+import Provider from '../../context/Provider';
 
 const renderPath = (path) => {
   const history = createBrowserHistory();
   history.push(path);
   const { ...resources } = render(
     <Router history={ history }>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </Router>,
   );
   return { ...resources, history };
