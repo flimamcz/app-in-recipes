d7a109fc95b30cae10e74cd982849e52b0901a27
Cristiane Kizelevicius <cristiane@samaan.com.br>
2022-10-24 22:34:15 -0300

    implementando inputs pagina login

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -3,12 +3,25 @@ import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
 function Provider({ children }) {
-  const [state, setState] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disable, setDisabled] = useState(true);
 
-  const contextValue = useMemo(() => ({
-    state,
-    setState,
-  }), [state]);
+  const contextValue = useMemo(
+    () => ({
+      email,
+      setEmail,
+      password,
+      setPassword,
+      disable,
+      setDisabled,
+    }),
+    [
+      disable,
+      email,
+      password,
+    ],
+  );
 
   return (
     <AppContext.Provider value={ contextValue }>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,78 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import AppContext from '../context/AppContext';
-// import PropTypes from 'prop-types';
 
 function Login() {
-  const { state } = useContext(AppContext);
+  const history = useHistory();
+  const { email,
+    setEmail,
+    password,
+    setPassword,
+    disabled,
+    setDisabled } = useContext(AppContext);
+
+  const validInput = () => {
+    const characterLength = 6;
+    const regex = /\S+@\S+\.\S+/;
+    const testEmail = email && regex.test(email);
+    const testPassword = password.length > characterLength;
+    if (testEmail && testPassword) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    };
+
+    useEffect(() => {
+      validInput();
+    });
+  
+
+  const handleClick = () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/meals');
+  };
+
+
   return (
-    <div>
-      <h3>Login</h3>
-      <p>{state}</p>
-    </div>
+    <section>
+      <form>
+        <h2 className="title-login">Login</h2>
+        <section className="section-input-login">
+          <input
+            type="Email"
+            value={ email }
+            data-testid="email-input"
+            placeholder="Digite se email"
+            onChange={ ({ target }) => setEmail(target.value) }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            onChange={ (({ target }) => setPassword(target.value)) }
+          />
+        </section>
+        <section className="section-button-login">
+          <button
+            type="button"
+            className="btn-login"
+            data-testid="login-submit-btn"
+            disable={ disabled }
+            onClick={ handleClick }
+          >
+            Enter
+          </button>
+        </section>
+
+      </form>
+    </section>
   );
 }
 
-// Login.propTypes = { key: PropTypes.type};
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Login;
