b40607de6b61733f7e0cef7728d0dc4a6ae0454f
VitorBarrioni <vitor_teixeira90@hotmail.com>
2022-10-25 19:12:44 -0300

    teste2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
+import AppContext from '../context/AppContext';
 
 export default function Header({ title, searchImage }) {
   const history = useHistory();
+  const { HandleChangeSearch, searchInput } = useContext(AppContext);
   const [search, setSearch] = useState(false);
   const searchClick = () => {
     setSearch(!search);
@@ -18,14 +20,30 @@ export default function Header({ title, searchImage }) {
     <div>
       <img data-testid="profile-top-btn" src={ profileIcon } alt="profile Icon" />
       <h1 data-testid="page-title">{title}</h1>
-      {search && <input data-testid="search-input" type="text" placeholder="Search" />}
+      {search && (
+        <input
+          data-testid="search-input"
+          type="text"
+          placeholder="Search"
+          value={ searchInput.search }
+          onChange={ HandleChangeSearch }
+        />
+      )}
 
       {searchImage && (
         <button type="button" onClick={ searchClick }>
-          <img data-testid="search-top-btn" src={ searchIcon } alt="search Icon" />
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="search Icon"
+          />
         </button>
       )}
-      <button data-testid="profile-top-btn" type="button" onClick={ profileClick }>
+      <button
+        data-testid="profile-top-btn"
+        type="button"
+        onClick={ profileClick }
+      >
         Profile
       </button>
       <SearchBar />
