2c7d5f06424708d3664b785cb4e1d5739e1ecaaf
Filipe <cimofelipe4@gmail.com>
2022-10-25 19:47:15 -0300

    Requisito 20 finalizado

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -4,7 +4,7 @@ import renderPath from './helpers/renderPath';
 import App from '../App';
 
 describe('Testa componente Recipes', () => {
-  it('Verifica se tem o texto Recipes na tela', () => {
+  it('Verifica se tem o texto Recipes na tela', async () => {
     const { history } = renderPath(<App />);
     const title = screen.getByRole('heading', { name: /login/i });
     expect(title).toBeInTheDocument();
@@ -12,5 +12,11 @@ describe('Testa componente Recipes', () => {
     act(() => {
       history.push('/meals');
     });
+    const titleMeals = screen.getByRole('heading', { name: /recipes/i });
+    expect(titleMeals).toBeInTheDocument();
+    const buttonProfile = screen.getByRole('button', { name: /profile/i });
+    expect(buttonProfile).toBeInTheDocument();
+    const buttonChicken = await screen.findByRole('button', { name: /chicken/i });
+    expect(buttonChicken).toBeInTheDocument();
   });
 });
