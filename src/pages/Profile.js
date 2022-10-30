ea2e7cd39deed125dc680b3871aa5c27563bdbba
Cristiane Kizelevicius <cristiane@samaan.com.br>
2022-10-30 06:41:16 -0300

    inserindo buttons pagina profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,9 +3,32 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';// import PropTypes from 'prop-types';
 
 function Profile() {
+  // const [emailProfile, setEmailProfile] = useState('');
   return (
     <div>
       <Header title="Profile" />
+      <h3 data-testid="profile-email">{emailProfile}</h3>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        name="done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        name="favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        name="profiel-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
