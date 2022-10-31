96632c802581c3b62bce502e543da1f3f1e9198a
julio-silveira <julioqueiroz15@gmail.com>
2022-10-31 14:47:17 -0300

    Página de Profile Finalizada

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';// import PropTypes from 'prop-types';
+import AppContext from '../context/AppContext';
 
 function Profile() {
   const history = useHistory();
   const [emailProfile, setEmailProfile] = useState('');
-  const { setEmail } = useContext(context);
+  console.log(emailProfile);
+  const { setEmail } = useContext(AppContext);
 
   const timeOut = 360;
 
@@ -19,7 +21,7 @@ function Profile() {
   return (
     <div>
       <Header title="Profile" />
-      <h3 data-testid="profile-email">{emailProfile}</h3>
+      <h3 data-testid="profile-email">{emailProfile.email}</h3>
       <button
         type="button"
         data-testid="profile-done-btn"
